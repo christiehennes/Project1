c0832810b9842b2df06079ece1563723cd9a9157
Christie Hennes <christiehennes@gmail.com>
2018-06-26 20:08:15 -0700

    Function for creating a concert object with location object

--- main.js
+++ main.js
@@ -1,92 +1,62 @@
 console.log('i am linked');
 
-
-
-// var map;
-//       function initMap() {
-//         map = new google.maps.Map(document.getElementById('map'), {
-//           zoom: 2,
-//           center: new google.maps.LatLng(2.8,-187.3),
-//           mapTypeId: 'terrain'
-//         });
-
-//         // Create a <script> tag and set the USGS URL as the source.
-//         var script = document.createElement('script');
-//         // This example uses a local copy of the GeoJSON stored at
-//         // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-//         script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-//         document.getElementsByTagName('head')[0].appendChild(script);
-//       }
-
-//       // Loop through the results array and place a marker for each
-//       // set of coordinates.
-//       window.eqfeed_callback = function(results) {
-//         for (var i = 0; i < results.features.length; i++) {
-//           var coords = results.features[i].geometry.coordinates;
-//           var latLng = new google.maps.LatLng(coords[1],coords[0]);
-//           var marker = new google.maps.Marker({
-//             position: latLng,
-//             map: map
-//           });
-//         }
-//       }
 //Variable Declarations
-let resultsOb = {
-    //Object to hold the results of the songkick API response, including artist info, num of concerts, and the concerts object 
-    artistName: '',
-    artistID: '',
-    artistsConcerts: [],
-    numConcerts: '',
-    locationsArray:[] //holds all the location objects to be used for google maps 
-}; 
+let resultsOb = {}; //function below to initalize 
 
 
 //Function Declarations
 
 
-// Function: api call for songkick + store result in an object 
+// Function: api call for JAMBASE + store result in an object 
 function getArtistInfo(artist){
+
+    //Clear out the information stored in the resultsOb
+    initResultsOb();
+
     //Create the API call 
     // Store the results in the skResults object 
     //Create a new concert for each concert (call the createConcert function in a loop)
   let artistID = `http://api.jambase.com/artists?name=${resultsOb.artistName}&page=0&api_key=d3zdba3y643smqmw5mn44wk8`;
 
-
-
-$.ajax({
-  url: artistID,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-  resultsOb.artistID = response.Artists[0].Id;
-  let eventByArtistId = `http://api.jambase.com/events?artistId=${resultsOb.artistID}&page=0&api_key=d3zdba3y643smqmw5mn44wk8`
-  $.ajax( {
-    url: eventByArtistId,
-    method: "GET"
-  }) .then(function(result) {
-    console.log(result);
-    resultsOb.numConcerts = result.Info.TotalResults;
-    result.Events.forEach(event => {
-      createConcert(event);
+    $.ajax({
+        url: artistID,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        resultsOb.artistID = response.Artists[0].Id;
+        let eventByArtistId = `http://api.jambase.com/events?artistId=${resultsOb.artistID}&page=0&api_key=d3zdba3y643smqmw5mn44wk8`
+        
+        $.ajax( {
+            url: eventByArtistId,
+            method: "GET"
+        }) .then(function(result) {
+            console.log(result);
+            resultsOb.numConcerts = result.Info.TotalResults;
+            result.Events.forEach(event => {
+                createConcert(event);
+            });
+            
+            
+        })
     });
-      
-    
-  })
-});
 }
 
 
 // Function: create concert object which will run in a loop for the number of concerts
 function createConcert(concert){
     //Takes the results object from the songkick API as an argument
-    //This function should run each time in a loop in the Songkick API for the number of concerts returned 
     //Create a new concert object and add to artistsConcerts array 
     //Use a map to return coordinate info and save in locationsArray --> https://repl.it/repls/KosherOrneryDeeplearning
 
-    let concert = {
-        locationName: '',
-        venueAddress: ''
-    }
+    artistsConcerts.push(concert); //Add the concert to the concerts array. This adds the FULL unprocessed object 
+
+    //Create the location oject 
+    let location = {
+        lat: concert.Venue.Latitude,
+        long: concert.Venue.Longitude,
+    };
+
+    locationsArray.push(loacation); //Add the location to the locations array 
 
 }
 
@@ -157,6 +127,17 @@ function displayConcertInfo(){
 
 // }
 
+function initResultsOb(){
+
+    resultsOb = {
+        //Object to hold the results of the songkick API response, including artist info, num of concerts, and the concerts object 
+        artistName: '',
+        artistID: '',
+        artistsConcerts: [],
+        numConcerts: '',
+        locationsArray:[] //holds all the location objects to be used for google maps 
+    }; 
+}
 
 //Click Handlers 
 
@@ -180,3 +161,7 @@ $(document).on('click', '.display', function(){
 
 
 })
+
+
+//On page load
+initResultsOb();
